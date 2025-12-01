6b0426e56de35f43bd774381343b078165912ed5
Lee <Leeolinger@kelowna.ca>
2025-11-30 22:22:10 -0800

    Initial Commit

---
+++ cabin critterTests/cabin_critterTests.swift
@@ -0,0 +1,17 @@
+//
+//  cabin_critterTests.swift
+//  cabin critterTests
+//
+//  Created by Lee  on 2025-11-30.
+//
+
+import Testing
+@testable import cabin_critter
+
+struct cabin_critterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
