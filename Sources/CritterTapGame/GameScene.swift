f1800cb39f2ac643f540453ca1e883b87929224a
Supraboy09 <Leeolinger@gmail.com>
2025-12-07 09:43:27 -0800

    Update GameScene.swift

--- Sources/CritterTapGame/GameScene.swift
+++ Sources/CritterTapGame/GameScene.swift
@@ -2,49 +2,56 @@ import SpriteKit
 
 class GameScene: SKScene {
 
-    private var scoreLabel: SKLabelNode!
-    private var score: Int = 0
+    private var scoreLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
+    private var score = 0
+    private let critterSize = CGSize(width: 80, height: 80)
 
     override func didMove(to view: SKView) {
         backgroundColor = .black
-        
-        scoreLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
-        scoreLabel.fontSize = 40
-        scoreLabel.position = CGPoint(x: size.width/2, y: size.height - 80)
-        scoreLabel.text = "Score: 0"
-        addChild(scoreLabel)
-
+        setupScoreLabel()
         spawnCritter()
     }
 
-    func spawnCritter() {
-        let critter = SKSpriteNode(color: .green, size: CGSize(width: 80, height: 80))
-        critter.position = CGPoint(
-            x: CGFloat.random(in: 50...(size.width - 50)),
-            y: CGFloat.random(in: 50...(size.height - 150))
-        )
+    private func setupScoreLabel() {
+        scoreLabel.fontSize = 40
+        scoreLabel.horizontalAlignmentMode = .center
+        scoreLabel.position = CGPoint(x: size.width / 2, y: size.height - 80)
+        scoreLabel.text = "Score: 0"
+        addChild(scoreLabel)
+    }
+
+    private func spawnCritter() {
+        // Only spawn one at a time
+        if childNode(withName: "critter") != nil { return }
+
+        let critter = SKSpriteNode(color: .green, size: critterSize)
         critter.name = "critter"
+
+        let safeInsets: CGFloat = 60
+        critter.position = CGPoint(
+            x: CGFloat.random(in: safeInsets...(size.width - safeInsets)),
+            y: CGFloat.random(in: safeInsets...(size.height - 150))
+        )
+
         addChild(critter)
 
         let wait = SKAction.wait(forDuration: 1.5)
         let remove = SKAction.removeFromParent()
-        critter.run(SKAction.sequence([wait, remove])) { [weak self] in
+        let sequence = SKAction.sequence([wait, remove])
+
+        critter.run(sequence) { [weak self] in
             self?.spawnCritter()
         }
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
-        let nodesTapped = nodes(at: location)
+        guard let touchLocation = touches.first?.location(in: self) else { return }
 
-        for node in nodesTapped {
-            if node.name == "critter" {
-                node.removeFromParent()
-                score += 1
-                scoreLabel.text = "Score: \(score)"
-                spawnCritter()
-            }
+        for node in nodes(at: touchLocation) where node.name == "critter" {
+            node.removeFromParent()
+            score += 1
+            scoreLabel.text = "Score: \(score)"
+            spawnCritter()
         }
     }
 }
