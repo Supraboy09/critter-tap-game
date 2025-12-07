a2558fb5f72963508d02054bf2f73c71059a453e
Supraboy09 <Leeolinger@gmail.com>
2025-12-06 19:35:19 -0800

    Update GameViewController.swift

--- Sources/CritterTapGame/GameViewController.swift
+++ Sources/CritterTapGame/GameViewController.swift
@@ -1,45 +1,17 @@
-//
-//  GameViewController.swift
-//  cabin critter
-//
-//  Created by Lee  on 2025-11-30.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
+        let skView = SKView(frame: view.bounds)
+        view.addSubview(skView)
 
-    override var prefersStatusBarHidden: Bool {
-        return true
+        let scene = GameScene(size: view.bounds.size)
+        scene.scaleMode = .resizeFill
+
+        skView.presentScene(scene)
     }
 }
