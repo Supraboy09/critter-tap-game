cab34f19b147b442a7c3587eee788edbb057ef29
Supraboy09 <Leeolinger@gmail.com>
2025-12-07 09:49:51 -0800

    Update GameScene.swift

--- Sources/CritterTapGame/GameScene.swift
+++ Sources/CritterTapGame/GameScene.swift
@@ -1,57 +1,96 @@
 import SpriteKit
 
+enum CritterType: CaseIterable {
+    case bunny
+    case fox
+    case bear
+    case raccoon
+    case goldenOwl
+
+    var imageName: String {
+        switch self {
+        case .bunny: return "bunny"
+        case .fox: return "fox"
+        case .bear: return "bear"
+        case .raccoon: return "raccoon"
+        case .goldenOwl: return "goldenowl"
+        }
+    }
+
+    var pointValue: Int {
+        switch self {
+        case .bunny: return 1
+        case .fox: return 2
+        case .bear: return 3
+        case .raccoon: return 4
+        case .goldenOwl: return 5
+        }
+    }
+
+    static func randomCritter() -> CritterType {
+        // weighted rarity: golden owl is harder to get
+        let normalCritters: [CritterType] = [.bunny, .fox, .bear, .raccoon]
+        let rareChance = Int.random(in: 1...15)
+
+        if rareChance == 1 { return .goldenOwl }
+        return normalCritters.randomElement()!
+    }
+}
+
 class GameScene: SKScene {
 
-    private var scoreLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
-    private var score = 0
-    private let critterSize = CGSize(width: 80, height: 80)
+    private var scoreLabel: SKLabelNode!
+    private var score: Int = 0
 
     override func didMove(to view: SKView) {
         backgroundColor = .black
-        setupScoreLabel()
+        
+        scoreLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
+        scoreLabel.fontSize = 40
+        scoreLabel.fontColor = .white
+        scoreLabel.position = CGPoint(x: size.width/2, y: size.height - 80)
+        scoreLabel.text = "Score: 0"
+        addChild(scoreLabel)
+
         spawnCritter()
     }
 
-    private func setupScoreLabel() {
-        scoreLabel.fontSize = 40
-        scoreLabel.horizontalAlignmentMode = .center
-        scoreLabel.position = CGPoint(x: size.width / 2, y: size.height - 80)
-        scoreLabel.text = "Score: 0"
-        addChild(scoreLabel)
-    }
+    func spawnCritter() {
+        let critterType = CritterType.randomCritter()
+        let texture = SKTexture(imageNamed: critterType.imageName)
 
-    private func spawnCritter() {
-        // Only spawn one at a time
-        if childNode(withName: "critter") != nil { return }
-
-        let critter = SKSpriteNode(color: .green, size: critterSize)
-        critter.name = "critter"
-
-        let safeInsets: CGFloat = 60
+        let critter = SKSpriteNode(texture: texture)
+        critter.size = CGSize(width: 120, height: 120)
         critter.position = CGPoint(
-            x: CGFloat.random(in: safeInsets...(size.width - safeInsets)),
-            y: CGFloat.random(in: safeInsets...(size.height - 150))
+            x: CGFloat.random(in: 60...(size.width - 60)),
+            y: CGFloat.random(in: 60...(size.height - 160))
         )
+        critter.name = critterType.imageName
+        critter.userData = ["points": critterType.pointValue]
 
         addChild(critter)
 
-        let wait = SKAction.wait(forDuration: 1.5)
+        let wait = SKAction.wait(forDuration: 1.6)
         let remove = SKAction.removeFromParent()
-        let sequence = SKAction.sequence([wait, remove])
-
-        critter.run(sequence) { [weak self] in
+        critter.run(SKAction.sequence([wait, remove])) { [weak self] in
             self?.spawnCritter()
         }
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touchLocation = touches.first?.location(in: self) else { return }
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
 
-        for node in nodes(at: touchLocation) where node.name == "critter" {
-            node.removeFromParent()
-            score += 1
-            scoreLabel.text = "Score: \(score)"
-            spawnCritter()
+        for node in tappedNodes {
+            if let sprite = node as? SKSpriteNode,
+               let points = sprite.userData?["points"] as? Int {
+
+                sprite.removeFromParent()
+                score += points
+                scoreLabel.text = "Score: \(score)"
+                spawnCritter()
+            }
         }
     }
 }
