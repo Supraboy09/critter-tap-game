aaeadec82686c49b28ab7adea4a0f590b1346169
Lee <Leeolinger@kelowna.ca>
2025-12-07 10:05:23 -0800

    Initial Commit

---
+++ CritterTapGameTests/CritterTapGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CritterTapGameTests.swift
+//  CritterTapGameTests
+//
+//  Created by Lee  on 2025-12-07.
+//
+
+import Testing
+@testable import CritterTapGame
+
+struct CritterTapGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
